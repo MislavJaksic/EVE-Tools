7495081cadb1fe833109dd9624e98fbf087c7721
MislavJaksic <jaksicmislav@gmail.com>
2021-04-22 23:34:56 +0200

    Updated the datastore, removed the libs and old LP sheet generator

--- eve_tools/runner.py
+++ eve_tools/runner.py
@@ -27,31 +27,6 @@ def main(args):
     dataframe = DataFrame("json", kills)
     dataframe.to_csv("kills.csv")
 
-    # with SQLiteConnection("./sde/sqlite-20210127.db") as db:
-    #     pass
-
-    # with EveCalculator() as calc:
-    #     with EveDatastore() as datastore:
-    #         derelik_blueprints = datastore.get_region_item_exchange_blueprints(
-    #             calc.region_name_to_id("Derelik")
-    #         )
-    # for contract_blueprint in derelik_blueprints:
-    #     print(contract_blueprint)
-
-    # dataframe = DataFrame("json", derelik_blueprints)
-    # dataframe.to_csv("derelik_blueprints.csv")
-
-    # lp_db_gen = LPDBGenerator()
-    # # lp_db_gen.to_csv("all-stores.csv")
-    # # save_unique_ids(lp_db_gen)
-    #
-    # ids = [int(x) for x in get_unique_ids(lp_db_gen)[0].tolist()]
-    # ids.sort()
-    # print(ids)
-    # getter = APIGetter()
-    # for id in ids:
-    #     print(getter.get_market_region_item_history(10000002, id))
-
 
 def run():
     """Entry point for the runnable script."""
